8e89414124620501a5568a0e2bad58734ef2dc7e
python <python@ubuntu.com>
2019-03-29 02:31:36 -0700

    add staitc resource

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import url
+from apps.user import views
+
 
 urlpatterns = [
+    url(r'^register/$', views.register, name='register'),
+    url(r'^register_handle$', views.register_handle, name='register_handle'),
+
 
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import re
+from user.models import User
+from django.core.urlresolvers import reverse
 
 # Create your views here.
+
+def register(request):
+    return render(request, 'register.html')
+
+
+def register_handle(request):
+    username = request.POST.get('user_name')
+    password = request.POST.get('pwd')
+    email = request.POST.get('email')
+    allow = request.POST.get('allow')
+    # print(username)
+    if not all([username, password, email]):
+        return render(request, 'register.html', {'errmsg':'shujuerror'})
+
+    if not re.match(r'^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+        return render(request, 'register.html', {'errmsg':'emailerror'})
+
+    if not allow == 'on':
+        return render(request, 'register.html', {'errmsg':'allowerror'})
+
+    # user = User()
+    # user.email = email
+    # user.password = password
+    # user.username = username
+    # user.save()
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        user = None
+
+    if user:
+        return render(request, 'register.html', {'errmsg': 'yonghumingyijchuunz'})
+
+
+    user = User.objects.create_user(username,email,password)
+    user.is_active = 0
+    user.save()
+
+    return redirect(reverse('goods:index'))
+
+
