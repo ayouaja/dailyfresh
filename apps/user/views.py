5b32301ac5b2f5f8f7724ea0a0904e27b92ab24b
python <python@ubuntu.com>
2019-04-01 01:58:12 -0700

    0401 submit

--- apps/user/views.py
+++ apps/user/views.py
@@ -2,46 +2,185 @@ from django.shortcuts import render, redirect
 import re
 from user.models import User
 from django.core.urlresolvers import reverse
+from django.views.generic import View
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from itsdangerous import SignatureExpired
+from django.conf import settings
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from celery_tasks.tasks import send_register_active_email
+
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
 
+
+        return render(request, 'register.html')
+
+    else:
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+        # print(username)
+        if not all([username, password, email]):
+            return render(request, 'register.html', {'errmsg': 'shujuerror'})
+
+        if not re.match(r'^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'errmsg': 'emailerror'})
+
+        if not allow == 'on':
+            return render(request, 'register.html', {'errmsg': 'allowerror'})
+
+        # user = User()
+        # user.email = email
+        # user.password = password
+        # user.username = username
+        # user.save()
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            user = None
+
+        if user:
+            return render(request, 'register.html', {'errmsg': 'yonghumingyijchuunz'})
+
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+
+        return redirect(reverse('goods:index'))
 
 def register_handle(request):
-    username = request.POST.get('user_name')
-    password = request.POST.get('pwd')
-    email = request.POST.get('email')
-    allow = request.POST.get('allow')
-    # print(username)
-    if not all([username, password, email]):
-        return render(request, 'register.html', {'errmsg':'shujuerror'})
+    pass
 
-    if not re.match(r'^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
-        return render(request, 'register.html', {'errmsg':'emailerror'})
+class RegisterView(View):
+    def get(selfs, request):
 
-    if not allow == 'on':
-        return render(request, 'register.html', {'errmsg':'allowerror'})
+        return render(request, 'register.html')
 
-    # user = User()
-    # user.email = email
-    # user.password = password
-    # user.username = username
-    # user.save()
-    try:
-        user = User.objects.get(username=username)
-    except User.DoesNotExist:
-        user = None
+    def post(self, request):
+        username = request.POST.get('user_name')
+        password = request.POST.get('pwd')
+        email = request.POST.get('email')
+        allow = request.POST.get('allow')
+        # print(username)
+        if not all([username, password, email]):
+            return render(request, 'register.html', {'errmsg': 'shujuerror'})
 
-    if user:
-        return render(request, 'register.html', {'errmsg': 'yonghumingyijchuunz'})
+        if not re.match(r'^[a-z0-9][\w\.\-]*@[a-z0-9\-]+(\.[a-z]{2,5}){1,2}$', email):
+            return render(request, 'register.html', {'errmsg': 'emailerror'})
+
+        if not allow == 'on':
+            return render(request, 'register.html', {'errmsg': 'allowerror'})
+
+        # user = User()
+        # user.email = email
+        # user.password = password
+        # user.username = username
+        # user.save()
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            user = None
+
+        if user:
+            return render(request, 'register.html', {'errmsg': 'yonghumingyijchuunz'})
+
+        user = User.objects.create_user(username, email, password)
+        user.is_active = 0
+        user.save()
+
+        # ji hua
+        serializer = Serializer(settings.SECRET_KEY, 3600)
+        info = {'confirm': user.id}
+        token = serializer.dumps(info)  # bytes
+        token = token.decode()
 
 
-    user = User.objects.create_user(username,email,password)
-    user.is_active = 0
-    user.save()
+        send_register_active_email.delay(email, username, token)
 
-    return redirect(reverse('goods:index'))
 
+        return redirect(reverse('goods:index'))
+
+
+class ActiveView(View):
+
+    def get(self, request, token):
+
+        print(token)
+
+        serializer = Serializer(settings.SECRET_KEY, 3600)
+        try:
+            info = serializer.loads(token)
+            # 获取待激活用户的id
+            user_id = info['confirm']
+
+            # 根据id获取用户信息
+            user = User.objects.get(id=user_id)
+            user.is_active = 1
+            user.save()
+
+            # 跳转到登录页面
+            return redirect(reverse('user:login'))
+        except SignatureExpired as e:
+            # 激活链接已过期
+            return HttpResponse('激活链接已过期')
+
+class LoginView(View):
+
+    def get(self, request):
+        # 判断是否记住了用户名
+        if 'username' in request.COOKIES:
+            username = request.COOKIES.get('username')
+            checked = 'checked'
+        else:
+            username = ''
+            checked = ''
+        # 使用模板
+        return render(request, 'login.html', {'username': username, 'checked': checked})
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('pwd')
+        if not all([username, password]):
+            return render(request, 'login.html', {'errmsg': '数据不完整'})
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            # 用户名密码正确
+            if user.is_active:
+                # 用户已激活
+                # 记录用户的登录状态
+                login(request, user)
+
+                # 获取登录后所要跳转到的地址
+                # 默认跳转到首页
+                next_url = request.GET.get('next', reverse('goods:index'))
+
+                # 跳转到next_url
+                response = redirect(next_url)  # HttpResponseRedirect
+
+                # 判断是否需要记住用户名
+                remember = request.POST.get('remember')
+
+                if remember == 'on':
+                    # 记住用户名
+                    response.set_cookie('username', username, max_age=7 * 24 * 3600)
+                else:
+                    response.delete_cookie('username')
+
+                # 返回response
+                return response
+            else:
+                # 用户未激活
+                return render(request, 'login.html', {'errmsg': '账户未激活'})
+        else:
+            # 用户名或密码错误
+            return render(request, 'login.html', {'errmsg': '用户名或密码错误'})
+
+        pass
 
